8aa1ab34933d706ce0701dbddaad94a0e39e7204
Jason Ford <fordjason@gmail.com>
2023-06-06 12:00:15 -0700

    Fix top level await

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import Player from './player.vue'
 
 window.Agent = browserAgent()
 
-const id = window.location.pathname.slice(1)
+async function initialize() {
+  const id = window.location.pathname.slice(1)
 
-if (validateUUID(id)) {
-  const question = await Agent.download(id).then(r => r.json())
-  vuePersistentComponent(Player, question, id)
+  if (validateUUID(id)) {
+    const question = await Agent.download(id).then(r => r.json())
+    vuePersistentComponent(Player, question, id)
+  }
+  else {
+    vuePersistentComponent(Customizer)
+  }
 }
-else {
-  vuePersistentComponent(Customizer)
-}
+
+initialize()
